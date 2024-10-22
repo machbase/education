5cc4f42da4eaae43ab345426064cdc23bd1027c0
yeonchun <118793092+mp3isnotacrime@users.noreply.github.com>
2024-10-22 15:02:11 +0900

    Update duplicate_check.sql

--- 05.No-Duplication/duplicate_check.sql
+++ 05.No-Duplication/duplicate_check.sql
@@ -2,17 +2,17 @@ DROP TABLE dup_tag;
 CREATE TAG TABLE dup_tag (name varchar(20) primary key, time datetime basetime, value double summarized) TAG_DUPLICATE_CHECK_DURATION=1;
 
 
-INSERT INTO dup_tag VALUES('tag1', '2024-01-01 09:00:00 000:000:001', 0);
-INSERT INTO dup_tag VALUES('tag1', '2024-01-02 09:00:00 000:000:001', 0);    
-INSERT INTO dup_tag VALUES('tag1', '2024-01-02 09:00:00 000:000:002', 0);
-INSERT INTO dup_tag VALUES('tag1', '2024-01-02 09:00:00 000:000:002', 1); -- Duplicate Data
-INSERT INTO dup_tag VALUES('tag1', '2024-01-03 09:00:00 000:000:003', 0);
+INSERT INTO dup_tag VALUES('tag1', '2024-10-01 09:00:00 000:000:001', 0);
+INSERT INTO dup_tag VALUES('tag1', '2024-10-02 09:00:00 000:000:001', 0);    
+INSERT INTO dup_tag VALUES('tag1', '2024-10-02 09:00:00 000:000:002', 0);
+INSERT INTO dup_tag VALUES('tag1', '2024-10-02 09:00:00 000:000:002', 1); -- Duplicate Data
+INSERT INTO dup_tag VALUES('tag1', '2024-10-03 09:00:00 000:000:003', 0);
 
-INSERT INTO dup_tag VALUES('tag2', '2024-01-04 09:00:00 000:000:001', 0);
-INSERT INTO dup_tag VALUES('tag2', '2024-01-04 09:00:00 000:000:001', 1); -- Duplicate Data
-INSERT INTO dup_tag VALUES('tag2', '2024-01-04 09:00:00 000:000:001', 2); -- Duplicate Data
-INSERT INTO dup_tag VALUES('tag2', '2024-01-04 09:00:00 000:000:002', 1);
-INSERT INTO dup_tag VALUES('tag2', '2024-01-04 09:00:00 000:000:003', 2);
+INSERT INTO dup_tag VALUES('tag2', '2024-10-04 09:00:00 000:000:001', 0);
+INSERT INTO dup_tag VALUES('tag2', '2024-10-04 09:00:00 000:000:001', 1); -- Duplicate Data
+INSERT INTO dup_tag VALUES('tag2', '2024-10-04 09:00:00 000:000:001', 2); -- Duplicate Data
+INSERT INTO dup_tag VALUES('tag2', '2024-10-04 09:00:00 000:000:002', 1);
+INSERT INTO dup_tag VALUES('tag2', '2024-10-04 09:00:00 000:000:003', 2);
 
 SELECT * FROM dup_tag where name = 'tag1';
 
